8afc1443b4e1797beeb8ba953f571d4bc847c67a
cecilia-tisca <ceciliatisca@gmail.com>
2022-06-12 08:23:33 -0500

    Feat: The Hoisting Array was created

---
+++ script.js
@@ -0,0 +1,11 @@
+/*HOISTING: Permite usar funciones y variables 
+antes de que se vaya declarado */
+
+/*CASO DE USO:Tenemor un Array con una variable
+no declarada y despues le asignamos el valor  */
+
+function nombre(name){
+    console.log("Mi nombre es " + name);
+}
+
+nombre("Diana");
