316943ae3d50f6248b6ab0d75e9a111663810829
cecilia-tisca <ceciliatisca@gmail.com>
2022-06-12 07:47:45 -0500

    Feat: The Unshift Array was created

---
+++ script.js
@@ -0,0 +1,12 @@
+/*UNSHIFT: Agrega uno o más elementos al 
+inicio del array, y devuelve la nueva longitud del array*/
+
+/*CASO DE USO: Tenemos un Array de personas
+Algunos se metieron a la fila y mover
+a los que recorrieron a tras cuando se metieron.*/
+
+const fila = ['Juan','Luis','Pedro','Moriana'];
+
+console.log(fila.unshift('Maria', 'Diana'));
+
+console.log(fila);
